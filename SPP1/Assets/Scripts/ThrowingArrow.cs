fec64248fb6995e94608d10cbc0d2f8e9f7f7817
ellietsamouli <elli.tsamouli@edu.futuregames.nu>
2023-12-16 22:22:54 +0100

    Revert "Revert "almost fixed the arrow""
    This reverts commit 3576fcb2a4305de41f283ec8e49dcfa6c285466f.

---
+++ SPP1/Assets/Scripts/ThrowingArrow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBullet : MonoBehaviour
+{
+    public Transform socket;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10;
+    public float delay = 1.0f; // Adjust the delay time as needed
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            StartCoroutine(SpawnBulletWithDelay());
+        }
+    }
+
+    IEnumerator SpawnBulletWithDelay()
+    {
+        yield return new WaitForSeconds(delay);
+
+        var bullet = Instantiate(bulletPrefab, socket.position, socket.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = socket.forward * bulletSpeed;
+    }
+}
