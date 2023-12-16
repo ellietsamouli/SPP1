a9ed1006021b6108ea75cddffe7ce45cbc1feda0
ellietsamouli <elli.tsamouli@edu.futuregames.nu>
2023-12-16 16:52:11 +0100

    Finished the wall shoot
    It only shoots if the face is opposite to the player, and only when the player is aligned with the wall

--- SPP1/Assets/Scripts/KnockOutObject.cs
+++ SPP1/Assets/Scripts/KnockOutObject.cs
@@ -6,36 +6,54 @@ public class KnockOutObject : MonoBehaviour
     public Animator playerAnimator;
     public GameObject cubePrefab;
     public float cubeSpeed = 5f;
-    public float delay = 1.0f; // Adjust the delay time as needed
+    public float delay = 1.0f;  // Adjust the delay time before moving the cube
+    public float disappearanceDelay = 3.0f;  // Adjust the delay time before destroying the cube
 
-    private bool cubeInstantiated = false;
+    private bool isShooting = false;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && !cubeInstantiated)
+        if (Input.GetKeyDown(KeyCode.E) && !isShooting)
         {
-            // Trigger the animation and instantiate the cube after the delay
-            StartCoroutine(ShootWithDelay());
+            Debug.Log("E key pressed");
+
+            // Trigger the animation and instantiate the cube with delays
+            StartCoroutine(ShootWithDelays());
         }
     }
 
-    private IEnumerator ShootWithDelay()
+    private IEnumerator ShootWithDelays()
     {
-        // Trigger the animation
+        isShooting = true;
+
+        // Trigger the animation with a delay
+        yield return new WaitForSeconds(delay);
         playerAnimator.SetTrigger("Shoot");
 
-        // Wait for the specified delay
-        yield return new WaitForSeconds(delay);
+        // Wait for the animation to complete before instantiating the cube
+        yield return new WaitForSeconds(0.1f);  // Adjust as needed
+
+        // Instantiate the cube
+        InstantiateCube();
+
+        isShooting = false;
+    }
+
+    private void InstantiateCube()
+    {
+        Debug.Log("Instantiating cube");
 
         // Instantiate a cube
         GameObject cube = Instantiate(cubePrefab, transform.position, Quaternion.identity);
 
-        // Apply force to the cube in the direction the player is looking at
-        Vector3 playerForward = transform.forward;
-        cube.GetComponent<Rigidbody>().AddForce(playerForward * cubeSpeed, ForceMode.Impulse);
+        // Calculate the opposite direction of the player
+        Vector3 oppositeDirection = -transform.forward;
 
-        // Set the flag to prevent multiple instantiations
-        cubeInstantiated = true;
+        // Apply force to the cube in the opposite direction
+        cube.GetComponent<Rigidbody>().AddForce(oppositeDirection * cubeSpeed, ForceMode.Impulse);
+
+        // Destroy the cube after a certain amount of time
+        Destroy(cube, disappearanceDelay);
     }
 
 
