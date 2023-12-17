a14d48ec5f740c4e4bcbdba924532632def7980d
ellietsamouli <elli.tsamouli@edu.futuregames.nu>
2023-12-17 18:14:48 +0100

    Arrow doesnt work
    Player gets destroyed when arrow appears

--- SPP1/Assets/Scripts/ArrowBullet1.cs
+++ SPP1/Assets/Scripts/ArrowBullet1.cs
@@ -9,12 +9,22 @@ public class ArrowBullet1 : MonoBehaviour
     private void Awake()
     {
         Destroy(gameObject, life);
-
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Destroy(collision.gameObject);
-        Destroy(gameObject);
+        // Check if the collided object is not the bullet itself
+        if (collision.gameObject != gameObject)
+        {
+            Destroy(collision.gameObject); // Destroy the collided object (assuming it's not the bullet)
+        }
+
+        // Check if the bullet is not colliding with the character
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject); // Destroy the bullet
+        }
     }
+
+
 }
